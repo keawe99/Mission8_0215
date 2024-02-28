504fec7eb896c118ef8462adb79dda6631d379a9
keawe99 <keawe1999@gmail.com>
2024-02-27 17:04:03 -0700

    first commit

--- Mission8_0215/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Mission8_0215.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
