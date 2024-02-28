c097fed4771bb7521e68bee7e6778b051c0bed19
keawe99 <keawe1999@gmail.com>
2024-02-28 09:28:58 -0700

    commit numero 3

--- Mission8_0215/Controllers/HomeController.cs
+++ Mission8_0215/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 //using Mission8_0215.Models;
 using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Mission8_0215.Controllers
 {
@@ -12,9 +13,55 @@ namespace Mission8_0215.Controllers
         {
         }
 
-        public IActionResult Task()
+        public IActionResult UserTaskForm()
         {
             return View();
+        }
+
+        //Take a look at home controller to make sure code matches to be able to add/edit
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var recordToEdit = _context.UserTaskQuadrant
+                .Single(x => x.MovieId == id);
+
+            ViewBag.category = _context.Categories //ViewBag stores small amounts of data to transfer to Views
+               .OrderBy(x => x.CategoryName)
+               .ToList();
+
+            //_context.SaveChanges();
+            return View("Quadrant", recordToEdit); //Returns them back to quadrant page with updated record
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Edit(Application UpdatedInfo) 
+        {
+            _context.Update(UpdatedInfo);
+            _context.SaveChanges();
+
+            return RedirectToAction("Confirmation");
+        }
+
+        [HttpGet]
+        public IActionResult Movies() //Change Movies and figure out where the user will be when they click this button and where you want them to go 
+        {
+            ViewBag.category = _context.Categories //ViewBag stores small amounts of data to transfer to Views
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Movies(Application response)
+        {
+            _context.Movies.Add(response); //Add record to database
+            _context.SaveChanges(); //commits changes to database
+
+            return View("UserTaskForm", response);
         }
 
 
